f2ff199ded9a0543a1a20074ee5d1531f63ffde6
GT <gaetantauvette@gmail.com>
2025-06-16 11:55:07 -0400

    Added command line example.

--- MLTrainer/Program.cs
+++ MLTrainer/Program.cs
@@ -20,8 +20,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        var mlContext = new MLContext();
 
+        //dotnet run --project MLTrainer
+
+        var mlContext = new MLContext();
         string dataPath = "ApiDocsData.csv";
 
         IDataView dataView = mlContext.Data.LoadFromTextFile<ApiDocData>(dataPath, hasHeader: true, separatorChar: ',');
@@ -32,9 +34,7 @@ class Program
             .Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
 
         var model = pipeline.Fit(dataView);
-
         var predictor = mlContext.Model.CreatePredictionEngine<ApiDocData, ApiDocPrediction>(model);
-
         var test = new ApiDocData { Text = "Use your bearer token in the Authorization header" };
         var result = predictor.Predict(test);
 
